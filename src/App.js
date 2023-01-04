8c605cc4f294a4fe19d28463d879f6a1453e43cf
Adinda Batubara <adindabatubara06@gmail.com>
2023-01-04 10:38:52 +0700

    ubah kedua

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => {
 }
 
 //  contoh apakah akan terpush hehehe
+// aku tambahin lagi untuk memastikan
 // function App() {
 //   // const adminUser={
 //   //   email:"adinda@gmail.com",
